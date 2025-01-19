ff8a5804c960d4e2004f147cf5326271f5a68880
QuistanC <Lisa@Kortkamp.de>
2025-01-19 22:45:19 +0100

    wip

--- NeuralNetwork/MainViewmodel.cs
+++ NeuralNetwork/MainViewmodel.cs
@@ -118,6 +118,7 @@ public partial class MainViewmodel : ObservableObject
 
         PredictionResult = $"Training completed. Generation: {CurrentGeneration}";
     }
+    //Refactor
 
     [RelayCommand]
     private void UploadImage()
